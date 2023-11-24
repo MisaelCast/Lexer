961a540cbbfa4726ee968cad35349001d0eebed7
Misael Castillo Rios <144559985+MisaelCast@users.noreply.github.com>
2023-11-24 15:38:46 -0800

    Update Sintactico100.js

--- Sintactico100.js
+++ Sintactico100.js
@@ -20,13 +20,9 @@ fs.readFile('tokensBuenos.txt', 'utf8', (err, data) => {
 });
 
 const reglas = {
-    655: [655],
-    1000: [1000],
-    3: [3],
-    655_1000_3: [655],
-    655_1000_3_1: [1000, 3, 1000, 3],
+    3: [3,1000],
+    655: [655, 1000 ],
     309: [309, 1000, 6],
-    6: [6],
 };
 
 function validaFrom() {
@@ -48,40 +44,7 @@ function validaFrom() {
 }
 
 function validaSelect() {
-    const reglaSelect = reglas[655];
-    const reglaColumna = reglas[1000];
-    const reglaComa = reglas[3];
 
-    let i = 1;
-
-    if (tokens.length < 3) {
-        console.log("Error en la estructura del SELECT: Estructura incompleta");
-        return;
-    }
-
-    if (tokens[1] !== reglaColumna[0]) {
-        console.log("Error en la estructura del SELECT: Se esperaba una columna después del SELECT");
-        return;
-    }
-
-    while (i < tokens.length - 1) {
-        if (tokens[i] === reglaColumna[0]) {
-            if (tokens[i + 1] === reglaComa[0]) {
-                i += 2;
-            } else if (tokens[i + 1] === reglaSelect[0] && tokens[i + 2] === reglaColumna[0]) {
-                i += 3;
-            } else if (tokens[i + 1] === reglas[309][0]) {
-                validaFrom();
-                return;
-            } else {
-                console.log("Error en la estructura del SELECT");
-                return;
-            }
-        } else {
-            console.log("Error en la estructura del SELECT");
-            return;
-        }
-    }
 
     console.log("Estructura del SELECT válida");
 }
